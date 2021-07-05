d15e26f8fa5a07a2d6a132b71a4deb5e5d943a2b
shifujulen <iamjulendiaz@gmail.com>
2021-07-05 20:48:05 +0200

    Make the drag and drop feature work with a few if's

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,119 @@
 const cards = document.querySelectorAll(".card");
 const stages = document.querySelectorAll(".stage__card-container");
 
-let draggedCard = null;
+let draggedCard;
 
-for (let i = 0; i < cards.length; i++) {
-  const card = cards[i];
+/* events fired on the draggable target */
+document.addEventListener("drag", function (event) {}, false);
 
-  card.addEventListener("dragstart", function () {
-    draggedCard = card;
+document.addEventListener(
+  "dragstart",
+  function (event) {
+    // store a ref. on the dragged elem
+    draggedCard = event.target;
+    // make it half transparent
+    event.target.style.opacity = 0.5;
+  },
+  false
+);
 
-    setTimeout(function () {
-      card.style.display = "none";
-    }, 0);
-  });
+document.addEventListener(
+  "dragend",
+  function (event) {
+    // reset the transparency
+    event.target.style.opacity = "";
+  },
+  false
+);
 
-  card.addEventListener("dragends", function () {
-    setTimeout(function () {
-      draggedCard.style.display = "block";
-      draggedCard = null;
-    }, 0);
-  });
+/* events fired on the drop targets */
+document.addEventListener(
+  "dragover",
+  function (event) {
+    // prevent default to allow drop
+    event.preventDefault();
+  },
+  false
+);
 
-  for (let j = 0; j < stages.length; j++) {
-    const stage = stages[j];
+document.addEventListener(
+  "dragenter",
+  function (event) {
+    // highlight potential drop target when the draggable element enters it
+    if (event.target.className == "dropzone") {
+      event.target.style.background = "purple";
+    }
+  },
+  false
+);
 
-    stage.addEventListener("dragover", function (e) {
-      e.preventDefault;
-    });
+document.addEventListener(
+  "dragleave",
+  function (event) {
+    // reset background of potential drop target when the draggable element leaves it
+    if (event.target.className == "stage__card-container") {
+      event.target.style.background = "";
+    }
+  },
+  false
+);
 
-    stage.addEventListener("dragenter", function (e) {
-      e.preventDefault;
-    });
+document.addEventListener(
+  "drop",
+  function (event) {
+    // prevent default action (open as link for some elements)
+    event.preventDefault();
+    console.log(event.target);
+    // move dragged elem to the selected drop target
+    if (event.target.className == "stage__card-container") {
+      event.target.style.background = "";
+      draggedCard.parentNode.removeChild(draggedCard);
+      event.target.appendChild(draggedCard);
+    } else if (
+      event.target.parentNode.className == "card" ||
+      event.target.parentNode.className == "stage__card-container"
+    ) {
+      event.target.style.background = "";
+      draggedCard.parentNode.removeChild(draggedCard);
+      event.target.parentNode.appendChild(draggedCard);
+    }
+  },
+  false
+);
+// for (let i = 0; i < cards.length; i++) {
+//   const card = cards[i];
 
-    stage.addEventListener("drop", function (e) {
-      this.append(draggedCard);
-      console.log(stages);
-    });
-  }
-}
+//   card.addEventListener("dragstart", function () {
+//     draggedCard = card;
+
+//     setTimeout(function () {
+//       card.style.display = "none";
+//     }, 0);
+//   });
+
+//   card.addEventListener("dragend", function () {
+//     setTimeout(function () {
+//       draggedCard.style.display = "flex";
+//       draggedCard = null;
+//     }, 0);
+//   });
+
+//   for (let j = 0; j < stages.length; j++) {
+//     const stage = stages[j];
+
+//     stage.addEventListener("dragover", function (e) {
+//       e.preventDefault;
+//       stage.style.backgroundColor = "pink";
+//     });
+
+//     stage.addEventListener("dragenter", function (e) {
+//       e.preventDefault;
+//     });
+
+//     stage.addEventListener("drop", function (e) {
+//       stage.append(draggedCard);
+
+//       console.log(e.target);
+//     });
+//   }
+// }
