2d42b1781b037c17df15a2cc3f0764fa87b0c55e
Nicolas Rios <dnicolas.r98@gmail.com>
2021-08-04 21:54:51 +0200

    Add form module

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ import UI from "./modules/ui.js";
 import Storage from "./modules/localStorage.js";
 import { Modals } from "./modules/modals.js";
 import { Listeners } from "./modules/listeners.js";
+import { Form } from "./modules/form.js";
 import dragger from "./modules/dragger.js";
 
 document.addEventListener("DOMContentLoaded", (e) => {
@@ -21,32 +22,4 @@ document.addEventListener("DOMContentLoaded", (e) => {
   Listeners.listenSupercheck();
 });
 
-document.querySelector(".form").addEventListener("submit", (e) => {
-  e.preventDefault();
-  const company = document.querySelector("#company").value.trim();
-  const position = document.querySelector("#position").value.trim();
-  const location = document.querySelector("#location").value.trim();
-  const offerLink = document.querySelector("#link").value.trim();
-  const isSuperJob = document.querySelector("#super").checked;
-  const date = new Date();
-  const stage = Modals.getCurrentStage();
 
-  if (Modals.validateForm()) {
-    const job = new Job(
-      company,
-      position,
-      location,
-      offerLink,
-      isSuperJob,
-      stage,
-      date
-    );
-    
-    //Save job into the localStorage
-    Storage.saveJob(job);
-    UI.addJobToStage(job);
-    UI.clearInputs();
-    Listeners.listenSupercheck();
-    Listeners.listenErrorImg()
-  }
-});
