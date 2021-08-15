c0b910f5ac13e270656131515d5c889a68c45564
Nicolas Rios <dnicolas.r98@gmail.com>
2021-08-15 11:58:07 +0200

    Turn dragger into an IIFE module

--- scripts/main.js
+++ scripts/main.js
@@ -4,21 +4,17 @@ import Storage from "./modules/localStorage.js";
 import { Modals } from "./modules/modals.js";
 import { Listeners } from "./modules/listeners.js";
 import { Form } from "./modules/form.js";
-import dragger from "./modules/dragger.js";
-
+import { Dragger } from "./modules/dragger.js";
 document.addEventListener("DOMContentLoaded", (e) => {
   //Fetch jobs from the localStorage and display them
   UI.displayJobs(Storage.getJobs());
-  //Initialize Dragger
-  dragger();
-  
+
   //Initialize Modals
   Modals.openModalListener();
   Modals.closeModalListener();
-  
+
   //Initialize Listeners
-  Listeners.listenErrorImg()
+  Listeners.listenErrorImg();
   Listeners.listenSupercheck();
   UI.deleteJobListener();
 });
-
